cedd913594623fc9da97706ffb76a7ba3baafe7d
tahaproo <44582827+tahaproo@users.noreply.github.com>
2018-11-09 22:01:11 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : mohamedtaha ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!crazy World`,"http://twitch.tv/S-F")
+client.user.setGame(`Crazy World`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
